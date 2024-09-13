0a1f802ec18c6eae8e535cd97d9f2bea54706439
Supernova1114 <supernovawarriors128@gmail.com>
2024-09-13 14:30:50 -0700

    cleaning up code

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@
 #include <memory>
 #include <chrono>
 #include <thread>
+#include <algorithm>
+#include <cstdlib>
 
 // Project to explore creating a 2D sandbox engine.
 // Particles can react to gravity, etc, can be mixed to create new elements.
 
+// TODO - move map and particle class to separate files from main.
+// TODO - combine empty check and bounds check into a function potentially.
 class Particle 
 {
 public:
@@ -41,8 +45,8 @@ public:
 
 private:
 
-    int pos_x; // Col
-    int pos_y; // Row
+    int pos_x;
+    int pos_y;
 
     std::string icon;
     
@@ -52,35 +56,41 @@ class Map2D
 {
 public:
 
+    // Constructor
     Map2D(int map_width, int map_height)
     {
         this->map_width = map_width;
         this->map_height = map_height;
 
+        instantiateMap(map_width, map_height);
+    }
+
+    // Instantiate map of size (map_width, map_height) with nullptr in each position.
+    void instantiateMap(int map_width, int map_height)
+    {
         map = std::vector<std::vector<std::shared_ptr<Particle>>>(map_height);
 
-        // Instantiate map of size map_width, map_height with nullptr.
-        for (int row = 0; row < map.size(); row++)
+        for (int row = 0; row < map_height; row++)
         {
             map[row] = std::vector<std::shared_ptr<Particle>>(map_width);
         
-            for (int col = 0; col < map[row].size(); col++)
+            for (int col = 0; col < map_width; col++)
             {
                 map[row][col] = nullptr;
-
             }
         }
     }
 
-    // Add particle to empty position.
-    bool addParticle(int x, int y)
+    // Add particle to an empty position.
+    // Returns true if successful.
+    bool addParticle(int x, int y, std::string icon)
     {
-        if (map[y][x] == nullptr)
+        if (isWithinMapBounds(x, y) && isPositionEmpty(x, y))
         {
-            auto particle = std::shared_ptr<Particle>(new Particle(x, y, "o"));
-            
-            particleList.push_back(particle);
+            auto particle = std::shared_ptr<Particle>(new Particle(x, y, icon));
             map[y][x] = particle;
+
+            particleList.push_back(particle);
 
             return true;
         }
@@ -88,9 +98,30 @@ public:
         return false;
     }
 
+    // Remove particle from (x,y) position.
+    bool removeParticle(int x, int y)
+    {
+        if (isWithinMapBounds(x, y))
+        {
+            if (!isPositionEmpty(x, y))
+            {
+                auto currentParticle = map[y][x];
+
+                map[y][x] = nullptr;
+                particleList.erase(std::remove(particleList.begin(), particleList.end(), currentParticle), particleList.end());
+            }
+
+            return true;
+        }
+        
+        return false;
+    }
+
+    // Move particle to empty location.
+    // Returns true if successful.
     bool moveParticle(std::shared_ptr<Particle> particle, int dest_x, int dest_y)
     {
-        if (isWithinMapBoundsXY(dest_x, dest_y) && map[dest_y][dest_x] == nullptr)
+        if (isWithinMapBounds(dest_x, dest_y) && isPositionEmpty(dest_x, dest_y))
         {
             map[particle->getY()][particle->getX()] = nullptr;
             particle->setPosition(dest_x, dest_y);
@@ -102,15 +133,29 @@ public:
         return false;
     }
 
+    // Check if position within map is empty.
+    bool isPositionEmpty(int x, int y)
+    {
+        if (map[y][x] == nullptr)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Print the current state of the map to the terminal.
     void printMap()
     {
-        for (int row = 0; row < map.size(); row++)
+        for (int row = 0; row < map_height; row++)
         {
-            for (int col = 0; col < map[row].size(); col++)
+            for (int col = 0; col < map_width; col++)
             {
-                if (map[row][col] != nullptr)
+                std::shared_ptr<Particle> currentParticle = map[row][col];
+
+                if (currentParticle != nullptr)
                 {
-                    std::cout << map[row][col]->getIcon();
+                    std::cout << currentParticle->getIcon();
                 }
                 else
                 {
@@ -119,13 +164,16 @@ public:
 
                 std::cout << " ";
             }
+
             std::cout << std::endl;
         }
     }
 
-    bool isWithinMapBoundsXY(int x, int y)
+    // Check if position is within map bounds.
+    bool isWithinMapBounds(int x, int y)
     {
-        if (isWithinMapBoundsX(x) && isWithinMapBoundsY(y))
+        if (x >= 0 && x < map_width
+            && y >= 0 && y < map_height)
         {
             return true;
         }
@@ -133,43 +181,10 @@ public:
         return false;
     }
 
-    bool isWithinMapBoundsX(int x)
-    {
-        if (x >= 0 && x < map[0].size())
-        {
-            return true;
-        }
-        
-        return false;
-    }
-
-    bool isWithinMapBoundsY(int y)
-    {
-        if (y >= 0 && y < map.size())
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    void testClear()
-    {
-        for (int row = map_height - 3; row < map_height; row++)
-        {
-            for (int col = 0; col < map_width; col++)
-            {
-                std::shared_ptr<Particle> particle = map[row][col];
-                //particleList.erase(std::remove(particleList.begin(), particleList.end(), particle), particleList.end());
-                map[row][col] = nullptr;
-                particle = nullptr;
-            }
-        }
-    }
-
+    // Get particle at (x,y) position.
     std::shared_ptr<Particle> getParticle(int x, int y)
     {
-        if (isWithinMapBoundsXY(x, y))
+        if (isWithinMapBounds(x, y))
         {
             return map[y][x];
         }
@@ -185,25 +200,39 @@ public:
     {
         for (std::shared_ptr<Particle> particle : particleList)
         {
+            
+
             //particle->update();
 
             int current_x = particle->getX();
             int current_y = particle->getY();
 
-            // See if below is clear
-            if (getParticle(current_x, current_y + 1) == nullptr)
+            // TODO - fix up stuffs
+
+            if (isWithinMapBounds(current_x, current_y + 1) && isPositionEmpty(current_x, current_y + 1))
             {
                 moveParticle(particle, current_x, current_y + 1);
             }
-            else if (getParticle(current_x - 1, current_y) == nullptr && getParticle(current_x - 1, current_y + 1) == nullptr)
+            else
             {
-                moveParticle(particle, current_x - 1, current_y);
+                // Choose between left or right at random.
+                int randomDir = rand() % 2; // 0 or 1
+
+                if (randomDir == 0)
+                {
+                    if (isWithinMapBounds(current_x - 1, current_y) && isWithinMapBounds(current_x - 1, current_y + 1) && isPositionEmpty(current_x - 1, current_y) && isPositionEmpty(current_x - 1, current_y + 1))
+                    {
+                        moveParticle(particle, current_x - 1, current_y);
+                    }
+                }
+                else
+                {
+                    if (isWithinMapBounds(current_x + 1, current_y) && isWithinMapBounds(current_x + 1, current_y + 1) && isPositionEmpty(current_x + 1, current_y) && isPositionEmpty(current_x + 1, current_y + 1))
+                    {
+                        moveParticle(particle, current_x + 1, current_y);
+                    }
+                }
             }
-            else if (getParticle(current_x + 1, current_y) == nullptr && getParticle(current_x + 1, current_y + 1) == nullptr)
-            {
-                moveParticle(particle, current_x + 1, current_y);
-            }
-            
         }
     }
 
@@ -234,18 +263,25 @@ int main()
 
     while (true)
     {
-        if (count % 2 == 0)
+        if (count % 5 == 0 && count < 2000)
         {
-            map.addParticle(5, 0);
+            map.addParticle(5, 0, "#");
             // map.addParticle(10, 0);
             // map.addParticle(15, 0);
-            map.addParticle(20, 0);
-            map.addParticle(25, 0);
+            map.addParticle(20, 0, "@");
+            map.addParticle(25, 0, "o");
             // map.addParticle(30, 0);
             // map.addParticle(35, 0);
-            map.addParticle(40, 0);
-            map.addParticle(45, 0);
+            map.addParticle(40, 0, "+");
+            map.addParticle(45, 0, "%");
         }
+
+        if (count % 10 == 0 && count > 1000)
+        {
+                map.removeParticle(25, 49);
+                map.removeParticle(24, 49);
+        }
+        
 
         // if (std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) - prevTime > timeout)
         // {
